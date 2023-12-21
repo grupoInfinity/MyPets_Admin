94f5b7966832e398e78867252d3921137cd21e3a
LUIS EDUARDO ALVARENGA CLAROS <20220298@ricaldone.edu.sv>
2023-12-21 12:19:02 -0600

    C

--- servicios/sec_usuario.php
+++ servicios/sec_usuario.php
@@ -1,5 +1,5 @@
 <?php
-include_once('../config.php'); 
+include_once('../config.php');
 
 $bd = "dbmypet";
 $tabla = "sec_usuario";
@@ -17,151 +17,147 @@ $estado = isset($_GET['estado']) ? $_GET['estado'] : '';
 $usercr = utf8_decode(isset($_GET['usercr']) ? $_GET['usercr'] : '');
 
 $json = "no has seteado nada.";
-function generarPin() {
-    // Generar un PIN de 6 dígitos
-    $pin = sprintf("%06d", mt_rand(0, 999999));
-    return $pin;
+function generarPin()
+{
+	// Generar un PIN de 6 dígitos
+	$pin = sprintf("%06d", mt_rand(0, 999999));
+	return $pin;
 }
 
-if(strtoupper($accion) =='C'){ //VERIFICACION SI LA ACCION ES CONSULTA
-	if(!empty($user)) $user="A.usr='$user'";
-	else $user="1=1";
-	if(!empty($nombre)) $nombre="AND A.nombre LIKE '%$nombre%'";
-	else $nombre="";
-	if(!empty($apellido)) $apellido="AND A.apellido LIKE '%$apellido%'";
-	else $apellido="";
-	if(!empty($estado)) $estado="AND A.estado='$estado'";
-	else $estado="";
-	if(!empty($pin)) $pin="AND A.pin='$pin'";
-	else $pin="";
-	
+if (strtoupper($accion) == 'C') { //VERIFICACION SI LA ACCION ES CONSULTA
+	if (!empty($user)) $user = "A.usr='$user'";
+	else $user = "1=1";
+	if (!empty($nombre)) $nombre = "AND A.nombre LIKE '%$nombre%'";
+	else $nombre = "";
+	if (!empty($apellido)) $apellido = "AND A.apellido LIKE '%$apellido%'";
+	else $apellido = "";
+	if (!empty($estado)) $estado = "AND A.estado='$estado'";
+	else $estado = "";
+	if (!empty($pin)) $pin = "AND A.pin='$pin'";
+	else $pin = "";
+
 	$sql = "
 	SELECT A.usr, A.clave, A.nombre, A.apellido, A.email, A.estado, A.pin
 	FROM $bd.$tabla A
 	WHERE $user $clave $estado $pin";
 
 	$result = $conn->query($sql);
-	
+
 	if (!empty($result))
-		if($result->num_rows > 0) {
-			while($row = $result->fetch_assoc()) {
+		if ($result->num_rows > 0) {
+			while ($row = $result->fetch_assoc()) {
 				$results[] = array(
-				"usuario" => $row["usuario"]
-				, 'clave' => utf8_decode($row["clave"])
-				, 'nombre' => utf8_decode($row["nombre"])
-				, 'apellido' => utf8_decode($row["apellido"])
-				, 'email' => utf8_decode($row["email"])
-				, 'pin' => utf8_decode($row["pin"])
-				, 'estado'=>$row["estado"]);
-				$json = array("status"=>1, "info"=>$results);
+					"usuario" => $row["usuario"],
+					'clave' => utf8_decode($row["clave"]), 
+					'nombre' => utf8_decode($row["nombre"]), 
+					'apellido' => utf8_decode($row["apellido"]), 
+					'email' => utf8_decode($row["email"]),
+					'pin' => utf8_decode($row["pin"]), 
+					'estado' => $row["estado"]
+				);
+				$json = array("status" => 1, "info" => $results);
 			}
 		} else {
-			$json = array("status"=>0, "info"=>"No existe información con ese criterio.");
+			$json = array("status" => 0, "info" => "No existe información con ese criterio.");
 		}
-	else $json = array("status"=>0, "info"=>"No existe información.");
-}
-else{
-	if(strtoupper($accion) =='I'){// VERIFICACION SI LA ACCION ES INSERCION
-		
+	else $json = array("status" => 0, "info" => "No existe información.");
+} else {
+	if (strtoupper($accion) == 'I') { // VERIFICACION SI LA ACCION ES INSERCION
+
 		//$date = date('Y-m-d');
-		$pin=generarPin();
-	
+		$pin = generarPin();
+
 		$sql = "INSERT INTO $bd.$tabla(usuario, clave, nombre, apellido, email,,pin ESTADO, USUARIO_CREACION/*, FECHA_CREACION*/) 
 		VALUE('$user','$clave', '$nombre', '$apellido', '$email', '$estado','$pin', '$usercr'/*, '$date'*/)";
-		
+
 		if ($conn->query($sql) === TRUE) {
-			$json = array("status"=>1, "info"=>"Registro almacenado exitosamente.");
+			$json = array("status" => 1, "info" => "Registro almacenado exitosamente.");
 		} else {
-			$json = array("status"=>0, "info"=>$conn->error);
+			$json = array("status" => 0, "info" => $conn->error);
 		}
-	}
-	else if(strtoupper($accion) =='U'){// VERIFICACION SI LA ACCION ES MODIFICACION
-	
-		if(!empty($clave)) $clave = "clave='".strtoupper($clave)."'";
+	} else if (strtoupper($accion) == 'U') { // VERIFICACION SI LA ACCION ES MODIFICACION
+
+		if (!empty($clave)) $clave = "clave='" . strtoupper($clave) . "'";
 		else $clave = "clave=clave";
-		if(!empty($nombre)) $nombre = ", nombre='".strtoupper($nombre)."'";
+		if (!empty($nombre)) $nombre = ", nombre='" . strtoupper($nombre) . "'";
 		else $nombre = ", nombre=nombre";
-		if(!empty($apellido)) $apellido = ", apellido='".strtoupper($apellido)."'";
+		if (!empty($apellido)) $apellido = ", apellido='" . strtoupper($apellido) . "'";
 		else $apellido = ", apellido=apellido";
-		if(!empty($email)) $email = ", email='".strtoupper($email)."'";
+		if (!empty($email)) $email = ", email='" . strtoupper($email) . "'";
 		else $email = ", email=email";
-		
-				
-		$estado = ", estado='".strtoupper($estado)."'";
-		$usercr = ", usuario_modificacion='".$usercr."'";
+
+		$pin=", pin=". generarPin() ."";
+		$estado = ", estado='" . strtoupper($estado) . "'";
+		$usercr = ", usuario_modificacion='" . $usercr . "'";
 		//$date = ", fecha_modificacion='".date('Y-m-d')."'";
-		
-		
-		$sql = "UPDATE $bd.$tabla SET $clave $nombre $apellido $email $estado $usercr /*$date*/ WHERE usr = '$user'";
-		
+
+
+		$sql = "UPDATE $bd.$tabla SET $clave $nombre $apellido $email $pin $estado $usercr /*$date*/ WHERE usr = '$user'";
+
 		echo $sql;
 		if ($conn->query($sql) === TRUE) {
-			$json = array("status"=>1, "info"=>"Registro actualizado exitosamente.");
+			$json = array("status" => 1, "info" => "Registro actualizado exitosamente.");
 		} else {
-			$json = array("status"=>0, "error"=>$conn->error);
+			$json = array("status" => 0, "error" => $conn->error);
 		}
-	}
-	else if(strtoupper($accion) =='D'){// VERIFICACION SI LA ACCION ES ELIMINACION
-		$usercr = ", usuario_modificacion='".$usercr."'";
+	} else if (strtoupper($accion) == 'D') { // VERIFICACION SI LA ACCION ES ELIMINACION
+		$usercr = ", usuario_modificacion='" . $usercr . "'";
 		//$date = ", fecha_modificacion='".date('Y-m-d')."'";
-		
+
 		$sql = "UPDATE $bd.$tabla set estado='I' $usercr /*$date*/ WHERE usuario = $user";
-		
+
 		if ($conn->query($sql) === TRUE) {
-			$json = array("status"=>1, "info"=>"Registro eliminado exitosamente.");
+			$json = array("status" => 1, "info" => "Registro eliminado exitosamente.");
 		} else {
-			$json = array("status"=>0, "error"=>$conn->error);
+			$json = array("status" => 0, "error" => $conn->error);
 		}
+	} else if (strtoupper($accion) == 'LU') { //VERIFICACION SI LA ACCION ES CONSULTA DEL LOGIN
+		if (!empty($usr)) $usr = "A.usuario='$user'";
+		else $user = "A.usuario = null";
+		if (!empty($estado)) $estado = "AND A.estado='A'";
+		else $estado = "AND A.estado=null";
+
+		$sql = "SELECT A.usuario FROM $bd.$tabla A WHERE $usr $estado ";
+
+		$result = $conn->query($sql);
+
+		if (!empty($result))
+			if ($result->num_rows > 0) {
+				while ($row = $result->fetch_assoc()) {
+					$results[] = array(
+						"user" => $row["usuario"]
+					);
+					$json = array("status" => 1, "info" => $results);
+				}
+			} else {
+				$json = array("status" => 0, "info" => "No existe información con ese criterio.");
+			}
+		else $json = array("status" => 0, "info" => "No existe información.");
+	} else if (strtoupper($accion) == 'LP') { //VERIFICACION SI LA ACCION ES CONSULTA DEL PASSWORD
+		if (!empty($user)) $user = "A.usuario='$user'";
+		else $user = "A.usuario = null";
+		if (!empty($clave)) $clave = "A.clave='$clave'";
+		else $clave = "AND A.clave = null";
+		if (!empty($estado)) $estado = "AND A.estado='A'";
+		else $estado = "AND A.estado=null";
+
+		$sql = "SELECT A.usuario FROM $bd.$tabla A WHERE $user $clave $estado ";
+
+		$result = $conn->query($sql);
+
+		if (!empty($result))
+			if ($result->num_rows > 0) {
+				while ($row = $result->fetch_assoc()) {
+					$results[] = array(
+						"user" => $row["usuario"]
+					);
+					$json = array("status" => 1, "info" => $results);
+				}
+			} else {
+				$json = array("status" => 0, "info" => "No existe información con ese criterio.");
+			}
+		else $json = array("status" => 0, "info" => "No existe información.");
 	}
-	else if(strtoupper($accion) =='LU'){ //VERIFICACION SI LA ACCION ES CONSULTA DEL LOGIN
-			if(!empty($usr)) $usr="A.usuario='$user'";
-			else $user = "A.usuario = null";
-			if(!empty($estado)) $estado="AND A.estado='A'";
-			else $estado="AND A.estado=null";
-			
-			$sql = "SELECT A.usuario FROM $bd.$tabla A WHERE $usr $estado ";
-
-			$result = $conn->query($sql);
-			
-			if (!empty($result))
-				if($result->num_rows > 0) {
-					while($row = $result->fetch_assoc()) {
-						$results[] = array(
-						"user" => $row["usuario"]
-						);
-						$json = array("status"=>1, "info"=>$results);
-					}
-				} else {
-					$json = array("status"=>0, "info"=>"No existe información con ese criterio.");
-				}
-			else $json = array("status"=>0, "info"=>"No existe información.");
-		}
-	else if(strtoupper($accion) =='LP'){ //VERIFICACION SI LA ACCION ES CONSULTA DEL PASSWORD
-			if(!empty($user)) $user="A.usuario='$user'";
-			else $user = "A.usuario = null";
-			if(!empty($clave)) $clave="A.clave='$clave'";
-			else $clave = "AND A.clave = null";
-			if(!empty($estado)) $estado="AND A.estado='A'";
-			else $estado="AND A.estado=null";
-			
-			$sql = "SELECT A.usuario FROM $bd.$tabla A WHERE $user $clave $estado ";
-
-			$result = $conn->query($sql);
-			
-			if (!empty($result))
-				if($result->num_rows > 0) {
-					while($row = $result->fetch_assoc()) {
-						$results[] = array(
-						"user" => $row["usuario"]
-						);
-						$json = array("status"=>1, "info"=>$results);
-					}
-				} else {
-					$json = array("status"=>0, "info"=>"No existe información con ese criterio.");
-				}
-			else $json = array("status"=>0, "info"=>"No existe información.");
-		}
-	
 }
 $conn->close();
 
@@ -170,4 +166,3 @@ header('Content-type: application/json');
 header('Access-Control-Allow-Origin: *');
 echo json_encode($json);
 //*/
- ?>
